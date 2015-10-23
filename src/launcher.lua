7fb2f0ee0df345d96266d069b7076f7d394062bf
denis <quux@ukr.net>
2015-10-23 16:48:33 +0300

    input parameters normalizer added

--- src/launcher.lua
+++ src/launcher.lua
@@ -7,46 +7,35 @@ local M = {}
 -- command line arguments list
 local arg = arg
 ---- imported module search path
---local search_path = package.path
 
-local print = print
 local string_format = string.format
-local os_exit =  os.exit
 local ipairs = ipairs
-local string_match = string.match
-local string_find = string.find
 local test_runner = require("test-runner")
--- test runner deps, should be imported before resetting path
-local lunit = require("lunit")
 
-
-
-
---_ENV = M
 
 local function main()
 
   if #arg < 1 then
     print(string_format("Usage: %s <absolute path to testmodule> [secondTestmodule]", arg[0]))
-    os_exit()
+    os.exit()
   end
 
   local testModuleLocation, fileName = M.extractArgumentsLocation(arg[1])
   local moduleUnderTestLocation = M.constructModuleLocation(testModuleLocation)
-  package.path = ""
+  package.path = package.path or ""
   package.path = package.path .. ";" .. testModuleLocation .. "/?.lua;"
-   .. moduleUnderTestLocation .. "/?.lua;"
-  
+    .. moduleUnderTestLocation .. "/?.lua;"
+
   -- forward test file names to test runner
   local testRunnerArguments = {}
-  
+
   for i, path in ipairs(arg) do
     local dir, name = M.extractArgumentsLocation(path)
-  	testRunnerArguments[#testRunnerArguments + 1] = name
+    testRunnerArguments[#testRunnerArguments + 1] = name
   end
-  
+
   test_runner.run(testRunnerArguments, package.path)
-  
+
 end
 ---------------------------------------------------------------------------
 -- Separates filename from file's location
@@ -56,24 +45,29 @@ end
 --                without extention
 function M.extractArgumentsLocation(path)
 
-  if not string_find(path, '(.*)/(*)') then
+  -- adding folder path, if absent
+  if not string.find(path, '(.*)/(*)') then
     path = './' .. path
   end
-  
+
+  -- adding extension, if not present
+  if not  string.find(path, '%.lua') then
+    path = path .. '.lua'
+  end
+
 
   local location, filenameWithExtension, filename =
-    string_match(path, '(.*)/(([^./]*)%....)')
+    string.match(path, '(.*)/(([^./]*)%....)')
 
   return location, filename
 end
--- print(string.match("/home/user/projects/dir/module.submodule.lua", '(.*)/((.*)%.[^%.][^%.][^%.])'))
 
 -------------------------------------------------------------------------------
 -- Constructs path to module under test source files, assuming that
 -- project has structure, when source file are placed under the src/ folder
 -- and tests are under the test/ folder under the same root.
 function M.constructModuleLocation(testSuiteLocation)
-  return string_match(testSuiteLocation, '(.*/)test') .. "src"
+  return string.match(testSuiteLocation, '(.*/)test') .. "src"
 end
 
 main()
