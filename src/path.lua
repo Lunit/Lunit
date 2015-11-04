5ee645734a4852c946fdbb27e5dd222c930d1a98
Denys Soroka <quux@ukr.net>
2015-11-04 15:30:20 +0200

    filenames with packages support added

--- src/path.lua
+++ src/path.lua
@@ -1,14 +1,16 @@
 ------------------------------
 -- Provide some functions for file path manipulation.
+-- Operates properly on filenames, that have no dots in their names except one
+-- separating fileanme from extension.
 -- @module path
 local M = {}
 
-  local string = string
+local string = string
 
 if setfenv then
-	setfenv(1, M) -- for 5.1
+  setfenv(1, M) -- for 5.1
 else
-	_ENV = M -- for 5.2
+  _ENV = M -- for 5.2
 end
 
 ---------------------------------------------------------------------------
@@ -20,20 +22,30 @@ end
 function M.extractArgumentsLocation(path)
 
   -- adding folder path, if absent
-  if not string.find(path, '(.*)/(*)') then
+  if not string.find(path, '(.*)/(.*)') then
     path = './' .. path
   end
 
   -- adding extension, if not present
-  if not  string.find(path, '%.lua') then
+
+  if not  string.find(path, '%.lua$') then
     path = path .. '.lua'
   end
 
 
   local location, filenameWithExtension, filename =
-    string.match(path, '(.*)/(([^./]*)%....)')
+    string.match(path, '(.*)/(([^/]*).lua)')
+
+  --  extract package name, if present
+  local package, moduleName = string.match(filenameWithExtension, '(.*)[.]([^.]*).lua')
+  if package then
+    location = location .. '/' .. string.gsub(package, '[.]', '/')
+    filename = moduleName
+  end
+
 
   return location, filename
 end
+
 
 return M
